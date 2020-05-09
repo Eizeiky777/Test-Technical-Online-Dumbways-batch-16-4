c2ac9767b39bfec56cb71e796824fd9dcac4b43e
Putu Ekky Crisayoga <62024374+Eizeiky777@users.noreply.github.com>
2020-05-09 21:15:18 +0700

    Add files via upload

---
+++ 1.hitung_camel_case.js
@@ -0,0 +1,12 @@
+// kode G untuk cek segala posibilitas kemunculan A_Z diikuti a-z 
+function checkCamelCase(x) {
+
+    let result = x.match(/[A-Z]+[^A-Z]*|[^A-Z]+/g);
+    return result;
+}
+
+
+var a = prompt("masukkan nama camelcase : ");
+var list_word = checkCamelCase(a);
+var length = checkCamelCase(a).length;
+document.write(length + ' -> terdiri dari (' + list_word + ')');
